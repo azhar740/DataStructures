feb957633b61b8978af9d6d25bce66b85e82854e
azhar740 <30354398+azhar740@users.noreply.github.com>
2022-09-06 00:36:38 +0530

    Update Citrix.cpp

--- ALGO & DS/Citrix.cpp
+++ ALGO & DS/Citrix.cpp
@@ -21,4 +21,19 @@
      3. we create the obj of that class and fill the fields value by case mapping 
      4. we write the data into avrofiles in some format e.g. rec_type and all their fields.
      5. We send that avro file to the end point(CAS Citrix analyics solutions).
+       
+       
+ Multithreading - 
+       
+       
+       
+  Autoscale Group Id Chnage -
+       
+                  Redis
+                  ||
+                  ADM Agent
 
+       
+       
+ Project-
+       Automated the analytics data pipeline(Automation of LogProxy code).
