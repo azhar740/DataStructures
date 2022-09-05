efa5274049ed40c2140258837a046876f2cbd938
azhar740 <30354398+azhar740@users.noreply.github.com>
2022-09-05 22:31:01 +0530

    Create Netapp.cpp

---
+++ ALGO & DS/Netapp.cpp
@@ -0,0 +1,31 @@
+Interview 1:
+*given a sorted doubly circular link list insert an element in such a way that resulting list will be sorted.
+
+*Networkig qustion
+What is TCP zero window, tcp sliding window
+*What are the loadbalancing methods apart from round robins
+*Consistent hashing
+*what is the system call, why it is required.
+
+
+Interview 2:
+*Given an array find the number of peaks e.g. 1 2 3 2 1 4 5 6 4 5
+number of peaks in a above array are 2
+
+*how consistent hashing ensures that requests will map unimormly on the circle.
+
+*how gdb are able to access the process address space since os concept is, one process can access the other process address
+space.
+
+
+Interview 3:
+*Multithreading: write a program in which one thread print the even numbers and the second print the odd numbers.
+*How you will implement set and get method for LRU cache what data structure you will use. Cant we use hashing instead of map in c++.
+
+Interview 4:
+*Related to projects in Riverbed technology.
+what is qos(quality of service, SNCF project), for what purpose we send system logs to another server(rsyslog feature, send logs using tcp, ssh instead of udp)
+what type of logs are there in system logs
+
+*Smart pointer concept 
+Implement smart pointer in C++, unique pointer, shared pointer.
