850e2384d2aa458e9e65ae4cfd9a6b6422bf9698
Azaruddhin Khan <Azaruddhin.Khan@netapp.com>
2024-10-01 00:50:51 +0530

    Update VolumeCalculator.java

--- SolidPrinciple/OCP/VolumeCalculator.java
+++ SolidPrinciple/OCP/VolumeCalculator.java
@@ -1,28 +1,7 @@
-import java.util.List;
-
-// Abstract class Shape
 abstract class Shape {
-    // Abstract method to calculate volume
-    abstract double volume();
+    public abstract double volume();
 }
 
-// Class Cone extends Shape
-class Cone extends Shape {
-    private double radius;
-    private double height;
-
-    public Cone(double radius, double height) {
-        this.radius = radius;
-        this.height = height;
-    }
-
-    @Override
-    double volume() {
-        return (1.0 / 3) * Math.PI * Math.pow(radius, 2) * height;
-    }
-}
-
-// Class Cuboid extends Shape
 class Cuboid extends Shape {
     private double length;
     private double width;
@@ -35,12 +14,11 @@ class Cuboid extends Shape {
     }
 
     @Override
-    double volume() {
+    public double volume() {
         return length * width * height;
     }
 }
 
-// Class Cylinder extends Shape
 class Cylinder extends Shape {
     private double radius;
     private double height;
@@ -51,53 +29,47 @@ class Cylinder extends Shape {
     }
 
     @Override
-    double volume() {
-        return Math.PI * Math.pow(radius, 2) * height;
+    public double volume() {
+        return Math.PI * radius * radius * height;
     }
 }
 
-// VolumeCalculator class
-public class VolumeCalculator {
+class Cone extends Shape {
+    private double radius;
+    private double height;
 
-    // Method to calculate the sum of volumes of a list of Shape objects
-    public double sumVolumeCalculator(List<Shape> shapes) {
+    public Cone(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double volume() {
+        return (Math.PI * radius * radius * height) / 3;
+    }
+}
+
+class VolumeCalculator {
+    public double sumVolume(Shape[] shapes) {
         double totalVolume = 0;
         for (Shape shape : shapes) {
             totalVolume += shape.volume();
         }
         return totalVolume;
     }
+}
 
-    // Violation: Modified method to include new shape types
-    public double sumVolumeCalculatorWithSpecificShapes(List<Object> shapes) {
-        double totalVolume = 0;
-        for (Object shape : shapes) {
-            if (shape instanceof Cone) {
-                totalVolume += ((Cone) shape).volume();
-            } else if (shape instanceof Cuboid) {
-                totalVolume += ((Cuboid) shape).volume();
-            } else if (shape instanceof Cylinder) {
-                totalVolume += ((Cylinder) shape).volume();
-            }
-            // Violation: Need to modify this method when adding a new shape
-//            else if (shape instanceof Sphere) {
-//                totalVolume += ((Sphere) shape).volume();
-//            }
-        }
-        return totalVolume;
-    }
-
+public class Main {
     public static void main(String[] args) {
-        Shape cone = new Cone(5, 10);
-        Shape cuboid = new Cuboid(4, 5, 6);
-        Shape cylinder = new Cylinder(3, 7);
+        Cuboid cuboid = new Cuboid(5, 3, 2);
+        Cylinder cylinder = new Cylinder(2, 4);
+        Cone cone = new Cone(3, 6);
+
+        Shape[] shapes = {cuboid, cylinder, cone};
 
         VolumeCalculator calculator = new VolumeCalculator();
-        List<Shape> shapes = List.of(cone, cuboid, cylinder);
+        double totalVolume = calculator.sumVolume(shapes);
 
-        System.out.println("Volume of Cone: " + cone.volume());
-        System.out.println("Volume of Cuboid: " + cuboid.volume());
-        System.out.println("Volume of Cylinder: " + cylinder.volume());
-        System.out.println("Total Volume: " + calculator.sumVolumeCalculator(shapes));
+        System.out.println("Total volume: " + totalVolume);
     }
 }
